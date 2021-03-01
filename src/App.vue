bedfe4319ee1d18fe1fa0e202680edd785c3ff0e
Andrés Osante <aosantea@ucenfotec.ac.cr>
2021-02-28 23:38:15 -0600

    add add taskr functionality

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <Header @btn-click="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask" />
+    <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask" />
     <div v-if="showAddTask">
       <AddTask @add-task="addTask" />
     </div>
